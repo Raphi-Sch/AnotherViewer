b17275cd00c18752b4ea0f96baa333a3c186562e
Raphi-Sch <35998957+Raphi-Sch@users.noreply.github.com>
2021-06-20 14:18:00 +0200

    + db handler

---
+++ src/db.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+var config = require('db.conf');
+
+var db = mysql.createConnection({
+    host: config["db_host"],
+    user: config["db_user"],
+    password: config["db_pass"],
+    database: config["db_name"]
+});
+
+db.connect(function(err) {
+    if(err) throw err;
+})
+
+function query(sql){
+    return new Promise(resolve => db.query(sql, function(err, result){
+        if (err) {
+            console.error(err);
+            resolve(null);
+        } 
+
+        resolve(result);
+    }));
+}
+
+module.exports = {query}
